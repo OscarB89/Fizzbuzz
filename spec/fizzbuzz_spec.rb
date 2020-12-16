7d3198fc2f9957fae37db8cc2af999c165a3403f
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2020-12-16 15:59:29 +0000

    Added rspec and updated fizzbuzz program logic to pass all test cases

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
-require_relative '../lib/fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+    it 'return 19 when passed 19' do
+      expect(fizzbuzz(19)).to eq 19
+    end
 end
